1c88d8df5f40db303c42db414248b4411e843baf
kit <kit@DESKTOP-EHKDJE1>
2025-08-30 23:03:16 +0300

    Distance

--- OOP/Point.cs
+++ OOP/Point.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿//#define POINT
+#define DISTANCE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOP;
 
 namespace OOP
 {
     internal class Point
     {
+#if POINT
         public double X { get; set; }
         public double Y { get; set; }
 
@@ -25,6 +29,30 @@ namespace OOP
         //Methods
         public void Print() { Console.WriteLine($" X = {X} Y = {Y}"); }
         //public void Print() { Console.WriteLine($" X = {GetX()} Y = {GetY()}"); }
+
+#endif
+#if DISTANCE
+        public double X { get; set; }
+        public double Y { get; set; }
+        //					Constructors:
+        public Point(double x = 0, double y = 0)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        //					Methods:
+        public double distance(Point other)
+        {
+            double x_distance = other.X - this.X;
+            double y_distance = other.Y - this.Y;
+            return Math.Sqrt(x_distance * x_distance + y_distance * y_distance);
+        }
+
+#endif
+
+
+
+
 
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -13,15 +13,17 @@ namespace OOP
             Point point = new Point();
             //point.SetX(2);
             //point.SetY(3);
-            point.Print();
+            //point.Print();
 
-            point.X = 7;
+            /*point.X = 7;
             point.Y = 8;
-            Console.WriteLine($" Properties: X = {point.X}, Y = {point.Y}");
+            Console.WriteLine($" Properties: X = {point.X}, Y = {point.Y}");*/
+            Point p1 = new Point(0,0);
+            Point p2 = new Point(18,16);
 
-
-
-
+            Console.WriteLine($" Properties p1: X = {p1.X}, Y = {p1.Y}");
+            Console.WriteLine($" Properties p2: X = {p2.X}, Y = {p2.Y}");
+            Console.WriteLine($"Растояние между точками: {p1.distance(p2)}");
 
 
         }
