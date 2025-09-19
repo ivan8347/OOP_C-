c294c3bff244892bd5f7c5d856ea3bc302eefa0a
kit <kit@DESKTOP-EHKDJE1>
2025-09-20 00:58:22 +0300

    girl Animation

---
+++ Geometry/Girl.cs
@@ -0,0 +1,151 @@
+﻿//#define STATIC
+#define ANIMATION
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    internal class Girl
+    {
+#if STATIC
+         public static void DrawGirl(Graphics g,int x,int y)
+    {
+
+        using (Pen pen = new Pen(Color.Black, 3))
+        { 
+            PointF A = new PointF(x , y);            //x =100    y=450
+            PointF B = new PointF(x + 100, y);
+            PointF C = new PointF(x + 50, y - 100);
+            PointF[] points = { A,B,C };
+            g.DrawPolygon(pen, points);
+        }
+        using (SolidBrush brush = new SolidBrush(Color.RoyalBlue))
+        {
+            PointF A = new PointF(x, y);
+            PointF B = new PointF(x + 100, y);
+            PointF C = new PointF(x + 50, y - 100);
+            PointF[] points = { A, B, C };
+            g.FillPolygon(brush, points);
+
+
+        }
+        using (SolidBrush brush = new SolidBrush(Color.Pink))
+        {
+            g.FillEllipse(brush, x + 20, y - 145, 60, 60);
+        }
+
+        using (Pen pen = new Pen(Color.Black, 3))
+        { 
+            g.DrawEllipse(pen, x + 20, y - 145, 60, 60);
+            g.DrawEllipse(pen, x + 40, y - 120, 5, 5);    //глаз
+            g.DrawEllipse(pen, x + 58, y - 120, 5, 5);     //глаз
+                g.DrawArc(pen, x + 40, y - 108, 20,10, 0, 180); // улыбка
+                g.DrawArc(pen, x + 45, y - 110, 10,3, 0, 180); // нос
+               g.DrawLine(pen, x + 35, y , x + 25,y +  50);  // правая нога
+               g.DrawLine(pen, x + 65, y , x + 75,y +  50);  // левая нога
+               g.DrawLine(pen, x + 27, y - 60,x - 5,   y - 80);  // правая рука
+               g.DrawLine(pen, x + 73, y - 60,x + 105, y - 80);  // левая рука
+            g.DrawEllipse(pen, x + 90, y - 200, 50, 60); //шар
+
+        }
+        using (Pen pen = new Pen(Color.White, 3)) 
+        { 
+            g.DrawLine(pen, x + 105,y - 80,x + 117,y - 143); 
+        }
+        using (Pen pen = new Pen(Color.Red, 8))
+        {
+            g.DrawLine(pen,x + 25,y + 50,x + 10,y + 50);  // правая нога
+            g.DrawLine(pen,x + 75,y + 50,x + 90,y + 50);  // левая нога
+        }
+        using (SolidBrush brush = new SolidBrush(Color.Red))   //шар
+        {
+            g.FillEllipse(brush, x + 90, y - 200, 50, 60);
+           // g.FillEllipse(brush, 120, 305, 60, 60);
+        }
+        using (Pen pen = new Pen(Color.OrangeRed, 20))
+        {
+            g.DrawArc(pen,x + 18,y - 142, 70, 100, 180, 180);
+
+
+        }    
+    }
+
+#endif
+
+#if ANIMATION
+        public static void DrawGirl(Graphics g, int x, int y, int size)
+        {
+            // Вместо жестких значений используем масштабный коэффициент
+            float scale = size / 100f; // Предположим, что размер 100 — базовый
+
+            using (Pen pen = new Pen(Color.Black, 3 * scale))
+            {
+                PointF A = new PointF(x, y);
+                PointF B = new PointF(x + 100 * scale, y);
+                PointF C = new PointF(x + 50 * scale, y - 100 * scale);
+                PointF[] points = { A, B, C };
+                g.DrawPolygon(pen, points);
+            }
+
+            using (SolidBrush brush = new SolidBrush(Color.RoyalBlue))
+            {
+                PointF A = new PointF(x, y);
+                PointF B = new PointF(x + 100 * scale, y);
+                PointF C = new PointF(x + 50 * scale, y - 100 * scale);
+                PointF[] points = { A, B, C };
+                g.FillPolygon(brush, points);
+            }
+
+            using (SolidBrush brush = new SolidBrush(Color.Pink))
+            {
+                g.FillEllipse(brush, x + 20 * scale, y - 145 * scale, 60 * scale, 60 * scale);
+            }
+
+            using (Pen pen = new Pen(Color.Black, 3 * scale))
+            {
+                // g.DrawEllipse(pen, x + 20 * scale, y - 145 * scale, 60 * scale, 60 * scale);
+                g.DrawArc(pen, x + 40 * scale, y - 108 * scale, 20 * scale, 10 * scale, 0, 180); // улыбка
+                g.DrawEllipse(pen, x + 40 * scale, y - 120 * scale, 5 * scale, 5 * scale);    // глаз
+                g.DrawEllipse(pen, x + 58 * scale, y - 120 * scale, 5 * scale, 5 * scale);     // глаз
+                g.DrawArc(pen, x + 45 * scale, y - 110 * scale, 10 * scale, 3 * scale, 0, 180); // нос
+                g.DrawLine(pen, x + 35 * scale, y, x + 25 * scale, y + 50 * scale);  // правая нога
+                g.DrawLine(pen, x + 65 * scale, y, x + 75 * scale, y + 50 * scale);  // левая нога
+                g.DrawLine(pen, x + 27 * scale, y - 60 * scale, x - 5 * scale, y - 80 * scale);  // правая рука
+                g.DrawLine(pen, x + 73 * scale, y - 60 * scale, x + 105 * scale, y - 80 * scale);  // левая рука
+                g.DrawEllipse(pen, x + 90 * scale, y - 200 * scale, 50 * scale, 60 * scale); //шар
+            }
+
+            using (Pen pen = new Pen(Color.White, 3 * scale))
+            {
+                g.DrawLine(pen, x + 105 * scale, y - 80 * scale, x + 117 * scale, y - 143 * scale);
+            }
+            using (Pen pen = new Pen(Color.Red, 3 * scale))
+            {
+            }
+            using (Pen pen = new Pen(Color.Red, 8 * scale))
+            {
+                g.DrawLine(pen, x + 25 * scale, y + 50 * scale, x + 10 * scale, y + 50 * scale);  // правая нога
+                g.DrawLine(pen, x + 75 * scale, y + 50 * scale, x + 90 * scale, y + 50 * scale);  // левая нога
+            }
+
+            using (SolidBrush brush = new SolidBrush(Color.Red))
+            {
+                g.FillEllipse(brush, x + 90 * scale, y - 200 * scale, 50 * scale, 60 * scale);
+            }
+
+            using (Pen pen = new Pen(Color.OrangeRed, 20 * scale))
+            {
+                g.DrawArc(pen, x + 18 * scale, y - 142 * scale, 70 * scale, 100 * scale, 180, 180);
+            }
+        }
+
+#endif
+
+    }
+}
--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -37,9 +37,9 @@ namespace Geometry
                     g.FillRectangle(Brushes.LightBlue, 0, 0, width, 300);
                     g.FillRectangle(Brushes.Green, 0, 300, width, 300);
 
-                    DrawingUtils.DrawCloud(g, 300, 100, 50);
-                    DrawingUtils.DrawCloud(g, 700, 60, 50);
-                    DrawingUtils.DrawCloud(g, 1000, 100, 70);
+                    Cloud.DrawCloud(g, 300, 100, 50);
+                    Cloud.DrawCloud(g, 700, 60, 50);
+                    Cloud.DrawCloud(g, 1000, 100, 70);
                     Circle.DrawCircle(g, 100, 100, 60, Color.Yellow, Color.Yellow, 3);
 
 
@@ -47,29 +47,32 @@ namespace Geometry
                     //var rectangle = new Rectangle(700, 450, 200, 150, Color.Violet, Color.Black, 3);
                     //rectangle.Fill(g);
                     //rectangle.Draw(g);
-
                     //var quadrilateral = new Quadrilateral(new Point(700, 300), new Point(850, 210), new Point(850, 340), new Point(700, 450), Color.Violet, Color.Black, 3);
                     //quadrilateral.Fill(g);
                     //quadrilateral.Draw(g);
                     Quadrilateral.DrawQuadrilateral(g, new Point(600, 250), new Point(750, 175), new Point(850, 210), new Point(700, 300), Color.Orange, Color.Black, 3);
                     Quadrilateral.DrawQuadrilateral(g, new Point(700, 300), new Point(850, 210), new Point(850, 340), new Point(700, 450), Color.Violet, Color.Black, 3);
                     Quadrilateral.DrawQuadrilateral(g, new Point(730, 380), new Point(830, 305), new Point(830, 260), new Point(730, 330), Color.White, Color.Black, 3);
+                     g.DrawLine(new Pen(Color.Black,3), 780, 343, 780, 295);
 
                     //var quadrilateral_1 = new Quadrilateral(new Point(600, 250), new Point(750, 175), new Point(850, 210), new Point(700, 300), Color.Orange, Color.Black, 3);
                     //quadrilateral_1.Fill(g);
                     //quadrilateral_1.Draw(g);
                     Triangle.DrawTriangle(g, new Point(500, 300), new Point(700, 300), new Point(600, 250), Color.Violet, Color.Black, 3);
+                    //Triangle.DrawTriangle(g, new Point(100, 450), new Point(200, 450), new Point(150, 350), Color.HotPink, Color.Black, 3);
 
                     //var triangle = new Triangle(new Point(500, 300), new Point(700, 300), new Point(600, 250), Color.Violet, Color.Black, 3);
                     //triangle.Fill(g);
                     //triangle.Draw(g);
 
                     Circle.DrawCircle(g, 600, 280, 10, Color.White, Color.Black, 3);
-
+                   // Circle.DrawCircle(g, 150, 320, 30, Color.Pink, Color.Black, 3);
+                    Girl.DrawGirl(g, 150, 450);
                     // var circle_1 = new Circle(600, 280, 10, Color.White, Color.Black, 3);
                     //circle_1.Fill(g); 
                     //circle_1.Draw(g); 
                     Rectangle.DrawRectangle(g, 550, 350, 100, 50, Color.White, Color.Black, 3);
+                     g.DrawLine(new Pen(Color.Black,3), 600, 350, 600, 400);
                     // var rectangle_1 = new Rectangle(650, 390, 100, 50, Color.White, Color.Black, 5);
                     // rectangle_1.Fill(g);
                     // rectangle_1.Draw(g);
@@ -107,22 +110,27 @@ namespace Animation
         private float cloud2X = 700;
         private float cloud3X = 1000;
         private float cloud4X = 500;
+        private float girlX = 1000;
+        private float girlY = 350;
+        private float targetGirlY = 450;
         private int width = 1200;
         private int height = 600;
         private int cloudSpeed = 1;
+        private int girlSpeed = 2;
+        private bool movingLeft = true;
 
         public MainForm()
         {
             this.Width = width;
             this.Height = height;
-          
+
 
             pictureBox = new PictureBox();
             pictureBox.Dock = DockStyle.Fill;
             this.Controls.Add(pictureBox);
 
             timer = new Timer();
-            timer.Interval = 50; 
+            timer.Interval = 50;
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -136,41 +144,67 @@ namespace Animation
                 g.FillRectangle(Brushes.LightBlue, 0, 0, width, 300);
                 g.FillRectangle(Brushes.Green, 0, 300, width, 300);
 
+                Circle.DrawCircle(g, 100, 100, 50, Color.Yellow, Color.Yellow, 3);
+                g.DrawLine(new Pen(Color.Yellow, 10), 100, 20, 100, 180);
+                g.DrawLine(new Pen(Color.Yellow, 10), 20, 100, 180, 100);
+                g.DrawLine(new Pen(Color.Yellow, 10), 160, 45, 45, 160);
+                g.DrawLine(new Pen(Color.Yellow, 10), 45, 45, 160, 160);
                 // Облака
                 Cloud.DrawCloud(g, (int)cloud1X, 100, 50);
                 Cloud.DrawCloud(g, (int)cloud2X, 60, 50);
                 Cloud.DrawCloud(g, (int)cloud3X, 100, 70);
                 Cloud.DrawCloud(g, (int)cloud4X, 50, 30);
+                // Girl.DrawGirl(g, (int)girl, 450,5);
+
 
                 Geometry.Rectangle.DrawRectangle(g, 500, 300, 200, 150, Color.Violet, Color.Black, 3);
                 Geometry.Rectangle.DrawRectangle(g, 550, 350, 100, 50, Color.White, Color.Black, 3);
+                g.DrawLine(new Pen(Color.Black, 3), 600, 350, 600, 400);
                 Triangle.DrawTriangle(g, new Point(500, 300), new Point(700, 300), new Point(600, 250), Color.Violet, Color.Black, 3);
                 Quadrilateral.DrawQuadrilateral(g, new Point(600, 250), new Point(750, 175), new Point(850, 210), new Point(700, 300), Color.Orange, Color.Black, 3);
                 Quadrilateral.DrawQuadrilateral(g, new Point(700, 300), new Point(850, 210), new Point(850, 340), new Point(700, 450), Color.Violet, Color.Black, 3);
                 Quadrilateral.DrawQuadrilateral(g, new Point(730, 380), new Point(830, 305), new Point(830, 260), new Point(730, 330), Color.White, Color.Black, 3);
-                Circle.DrawCircle(g, 100, 100, 50, Color.Yellow, Color.Yellow, 3);
+                g.DrawLine(new Pen(Color.Black, 3), 780, 343, 780, 295);
                 Circle.DrawCircle(g, 600, 280, 10, Color.White, Color.Black, 3);
+
+              
+
+                // Обновляем изображение PictureBox
+                pictureBox.Image?.Dispose();
+                pictureBox.Image = bmp;
+
+                // Обновляем позиции облаков
+                cloud1X += cloudSpeed;
+                cloud2X += cloudSpeed;
+                cloud3X += cloudSpeed;
+                cloud4X += cloudSpeed;
+                girlX -= girlSpeed;
+                girlY += girlSpeed;
+
+                float size = 30 + (girlY - 250) * 0.3f; // например, базовый 30, увеличивается с girlY
+                if (size < 30) size = 10; // минимальный размер
+                if (size > 100) size = 100; // максимум
+                int girlSize = (int)size;
+
+                float deltaY = targetGirlY - girlY;
+                girlY += deltaY * 0.01f;
+                if (girlY > 450)
+                {
+                    targetGirlY = 220; // девочка поднимается вверх
+                }
+               
+                // Циклический эффект
+                if (cloud1X > width + 50) cloud1X = -50;
+                if (cloud2X > width + 50) cloud2X = -50;
+                if (cloud3X > width + 50) cloud3X = -50;
+                if (cloud4X > width + 50) cloud4X = -50;
+
+                if (girlX < 50) ;
+                if (girlY > 300) ;
+                Girl.DrawGirl(g, (int)girlX, (int)girlY, (int)size);
+
             }
-
-            // Обновляем изображение PictureBox
-            pictureBox.Image?.Dispose();
-            pictureBox.Image = bmp;
-
-            // Обновляем позиции облаков
-            cloud1X += cloudSpeed;
-            cloud2X += cloudSpeed;
-            cloud3X += cloudSpeed;
-            cloud4X += cloudSpeed;
-
-            // Циклический эффект
-            if (cloud1X > width + 50) cloud1X = -50;
-            if (cloud2X > width + 50) cloud2X = -50;
-            if (cloud3X > width + 50) cloud3X = -50;
-            if (cloud4X > width + 50) cloud4X = -50;
         }
-
-
-
         static void Main()
         {
             Application.EnableVisualStyles();
